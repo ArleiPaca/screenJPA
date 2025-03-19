67eec84a406020958599b0a2e705c90dfe0dab5d
ArleiPaca <aplepiani@gmail.com>
2025-03-18 23:14:56 -0300

    ajustes

--- src/main/java/br/com/arlei/screenmatch/service/ConsultaChatGPT.java
+++ src/main/java/br/com/arlei/screenmatch/service/ConsultaChatGPT.java
@@ -5,7 +5,7 @@ import com.theokanning.openai.service.OpenAiService;
 
 public class ConsultaChatGPT {
     public static String obterTraducao(String texto) {
-        OpenAiService service = new OpenAiService("cole aqui sua chave da OpenAI");
+        OpenAiService service = new OpenAiService("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");
 
         CompletionRequest requisicao = CompletionRequest.builder()
                 .model("gpt-3.5-turbo-instruct")
